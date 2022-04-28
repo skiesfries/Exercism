4b4c67956fddb183f0fa1e8b6205b2536cccac34
Sky Ortiz <tortiz942@gmail.com>
2022-04-28 10:57:34 -0400

    sum of multiples
    complete

--- Exercism/SumOfMultiples.cs
+++ Exercism/SumOfMultiples.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace Exercism
 {
-    class SumOfMultiples
+    public static class SumOfMultiples
     {
+        public static int Sum(IEnumerable<int> multiples, int max)
+        {
+            var products = new List<int>();
+            foreach (var multiple in multiples)
+            {
+                if (multiple > 0)
+                {
+                    for (int i = 1; i * multiple < max; i++)
+                    {
+                        products.Add(i * multiple);
+                    }
+                }
+            }
+            return products.Distinct().Sum();
+        }
     }
 }
