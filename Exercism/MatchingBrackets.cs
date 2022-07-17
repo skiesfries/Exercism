16c23e0bee20819c327afd1d7f8b58218c33011b
Sky Ortiz <tortiz942@gmail.com>
2022-07-17 18:47:20 -0400

    started matching brackets challenge

---
+++ Exercism/MatchingBrackets.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercism
+{
+    public static class MatchingBrackets
+    {
+        private static char[] openingbrackets = { '{', '[', '(' };
+        private static char[] closedbrackets = {'}',']',')' };
+
+        public static bool IsPaired(string input)
+        {
+            if (AreSquareBracketsPaired(input))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static bool AreSquareBracketsPaired(string input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '[')
+                {
+                   
+                }
+            }
+            return false;
+        }
+    }
+}
--- Exercism/Program.cs
+++ Exercism/Program.cs
@@ -9,7 +9,7 @@ namespace Exercism
     {
         static void Main(string[] args)
         {
-            WriteLine(RotationalCipher.Rotate("omg", 5));
+            WriteLine(MatchingBrackets.IsPaired("[hel[lo]"));
 
         }
     }
